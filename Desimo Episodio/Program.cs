aff53d8dce5d76eb966cfd46ca29925e73246aad
Angel19cj <85655105+Angel19cj@users.noreply.github.com>
2025-01-17 12:42:47 -0400

    Update Program.cs

--- Desimo Episodio/Program.cs
+++ Desimo Episodio/Program.cs
@@ -4,29 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _10_DatesAndTimes
+namespace desimoEpisodio
 {
     class Program
     {
         static void Main(string[] args)
         {
             DateTime myValue = DateTime.Now;
-            //Console.WriteLine(myValue.ToString());
-            //Console.WriteLine(myValue.ToShortDateString());
-            //Console.WriteLine(myValue.ToShortTimeString());
-            //Console.WriteLine(myValue.ToLongDateString());
-            //Console.WriteLine(myValue.ToLongTimeString());
 
-            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
-            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
-            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
-
-            //Console.WriteLine(myValue.Month);
-
-            //DateTime myBirthday = new DateTime(1969, 12, 7);
-            //Console.WriteLine(myBirthday.ToShortDateString());
-
-            DateTime myBirthday = DateTime.Parse("12/7/1969");
+            DateTime myBirthday = DateTime.Parse("06/7/2000");
             TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
             Console.WriteLine(myAge.TotalDays);
 
