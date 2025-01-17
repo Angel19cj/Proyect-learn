d55902bc94029893ca69dbdda09ac628ad3d9919
angel19cj <angel19cj2@gmail.com>
2025-01-17 12:12:59 -0400

    Se agrega el proyecto

---
+++ Tercer episodio (variables)/Variables/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x+3;
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+            Console.WriteLine("Cual es tu nombre?");
+            Console.WriteLine("Tu primer nombre");
+            string myfristname;
+            myfristname= Console.ReadLine();
+
+            string mylastname;
+            Console.WriteLine("Tu apellido");
+            mylastname= Console.ReadLine();
+
+            Console.WriteLine("Hello " + myfristname+ " " + mylastname);
+            Console.ReadLine();
+
+        }
+    }
+}
